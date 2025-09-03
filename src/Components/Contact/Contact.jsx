b4d50cfa8ab7a6e99d6756df49ed627490c820b1
Eniekpo <johneniekpo4life@gmail.com>
2025-09-03 01:15:07 +0100

    a

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './Contact.css'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+import theme_pattern from '../../assets/theme_pattern.svg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className="contact-title">
+        <h1>Get in touch</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>I'm a frontend developer from Nigeria with 10 years of experience in multiple Components like microsoft, telsa and Apple</p>
+          <div className="contact-details">
+
+            <div className="contact-detai">
+              <img src={mail_icon} alt="" />
+              <p>yoletech@gmail.com</p>
+            </div>
+
+            <div className="contact-detai">
+              <img src={call_icon} alt="" />
+              <p>+2347032767758</p>
+            </div>
+
+            <div className="contact-detai">
+              <img src={location_icon} alt="" />
+              <p>Location</p>
+            </div>
+
+          </div>
+        </div>
+
+        <form action="" className='contact-right'>
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder='Enter your name' name='name' />
+          <label htmlFor="">Your Email</label>
+          <input type="email" placeholder='Enter your email' name='email' />
+          <label htmlFor="">Write your message here</label>
+          <textarea name="message" rows='6' placeholder='Enter your message'></textarea>
+          <button type='submit' className="contact-submit">Submit Now</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>I'm a frontend developer from Nigeria with 10 years of experience in multiple Components like microsoft, telsa and Apple</p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder='Enter your email' />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">@copy; @2025 yoltetech, All rights reserved, all wrongs avenged</p>
+        <div className="footer-bottom-right">
+          <p>Term of Services</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
